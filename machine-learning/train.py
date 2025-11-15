1d05db9049bf55cbe82415d29a6e1be1ad47afd6
Francesco Capuano <francesco.capuano@huggingface.co>
2025-11-15 16:46:33 +0000

    add: optimizer API and scheduling

--- machine-learning/train.py
+++ machine-learning/train.py
@@ -5,20 +5,21 @@ from flow_matching import MiniFlowMLP
 from dataset import MiniEEG2MEGDataloader
 from optimization import MiniOptimizer
 from tqdm import tqdm
+from utils import ModelParameters
 
-ModelParameters = dict
 
 def train_epoch(
     model: MiniFlowMLP,
     params: ModelParameters,
     dataloader: MiniEEG2MEGDataloader,
     optimizer: MiniOptimizer,
-    key: jax.random.PRNGKey = jax.random.PRNGKey(42)
+    global_state: dict,
+    key: jax.random.PRNGKey = jax.random.PRNGKey(42),
 ) -> ModelParameters:
-    lr = 1e-3
     # total = total number of iterations you expect
     total_steps = len(dataloader.dataset) // dataloader.batch_size
     progress_bar = tqdm(total=total_steps, desc="Training")
+    training_step = global_state["training_step"]
 
     for batch in dataloader:
         x = batch["meg"]  # extracting the meg part only and training a FM model on that
@@ -29,13 +30,14 @@ def train_epoch(
         grads = grad_loss_function(params)
 
         # Perform a (dummy) update step using gradients
-        params = jax.tree.map(lambda p, g: p - lr * g, params, grads)
-        # params = optimizer.update(params=params, grad=grad, step=i)
+        params = optimizer.update(params=params, grad=grads, step=training_step)
+        training_step += 1
         progress_bar.update(1)
 
+    global_state["training_step"] = training_step
     progress_bar.close()
 
-    return params
+    return params, global_state
 
 def eval_epoch(
         model: MiniFlowMLP,
@@ -98,17 +100,30 @@ def main():
 
     params = flow_mlp.init_params(key=key)
 
+    n_epochs = 5  # minimal training
+
+    optimizer_config = dict(
+        name = "sgd",
+        total_steps = (n_epochs - 1) * len(train_dataloader),
+        warmup_steps = len(train_dataloader),
+    )
+
+    training_config["optimizer_config"] = optimizer_config
     wandb.init(project="eeg2meg", config=training_config)
 
-    optimizer = None  # currently only implementing basic gradient descent
-    n_epochs = 5  # minimal training
+    optimizer = MiniOptimizer(**optimizer_config)
+
     eval_every = 1
+    training_step = 0
+    global_state = {"training_step": training_step}
 
     for epoch in range(n_epochs):
-        params = train_epoch(flow_mlp, params, train_dataloader, optimizer, train_key)
+        params, global_state = train_epoch(flow_mlp, params, train_dataloader, optimizer, global_state, train_key)
 
         if epoch % eval_every == 0:
             eval_epoch(flow_mlp, params, eval_dataloader, eval_key)
+        
+        training_step += 1
 
 if __name__ == "__main__":
     main()
