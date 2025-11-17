1b22bdbea9042c9780c438e162b6c3d82d636c55
Francesco Capuano <francesco.capuano@huggingface.co>
2025-11-17 11:42:42 +0000

    add: adamw

--- machine-learning/train.py
+++ machine-learning/train.py
@@ -112,10 +112,10 @@ def main():
     eval_every = 3
 
     optimizer_config = dict(
-        name = "adam",
+        name = "adamw",
         total_steps = (n_epochs - 1) * len(train_dataloader),
         warmup_steps = len(train_dataloader),
-        kwargs={"beta": 0.99, "gamma": 0.9}
+        kwargs={"beta": 0.99, "gamma": 0.9, "lambda_wd": 1e-2}
     )
 
     training_config["optimizer_config"] = optimizer_config
